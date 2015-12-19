26886afc406cfd100a732ba715614a4040a2ec8f
Hironori Kawano <programmingpapa.com@gmail.com>
2015-12-19 10:50:25 +0900

    Added bootstrap and fontawesome using bower

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,19 @@ var elixir = require('laravel-elixir');
  |
  */
 
+var paths = {
+  'jquery': './vendor/bower_components/jquery/',
+  'bootstrap': './vendor/bower_components/bootstrap-sass/assets/',
+  'fontawesome': './vendor/bower_components/font-awesome/'
+}
+
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass("app.scss", 'public/css/', {includePaths: [paths.bootstrap + 'stylesheets/', paths.fontawesome + 'scss/']})
+        .copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
+        .copy(paths.fontawesome + 'fonts/**', 'public/fonts')
+        .scripts([
+            'app.js',
+            paths.jquery + "dist/jquery.js",
+            paths.bootstrap + "javascripts/bootstrap.js"
+        ]);
 });
